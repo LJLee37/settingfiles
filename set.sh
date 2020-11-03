f5169a100155a63e79062f58e4341c60e0f6bc7c
LJLee37 <ljlee3759@gmail.com>
2020-11-03 10:49:31 +0900

    macOS setting

--- set.sh
+++ set.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+brew install neovim tmux git yarn minecraft
+python3 -m pip install neovim
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 # p10k configure
@@ -7,8 +9,8 @@ ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
 cp ~/.tmux/.tmux.conf.local ~/
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -sL install-node.now.sh/lts | sudo bash
-curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+#curl -sL install-node.now.sh/lts | sudo bash
+#curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 mkdir -P ~/.config/nvim
 cp ~/gitRepos/settingfiles/init.vim ~/.config/nvim/init.vim
 mv ~/.zshrc ~/.zshrc.bak

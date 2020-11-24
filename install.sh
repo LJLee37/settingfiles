86d5e5af21e55d6370aa732a31eeec074714b553
LJLee37 <ljlee3759@gmail.com>
2020-11-24 13:34:41 +0900

    add ctags

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install zsh tmux neovim python3-neovim python3-pip htop git g++ gdb neofetch -y
+sudo apt install zsh tmux neovim ctags python3-neovim python3-pip htop git g++ gdb neofetch -y
 

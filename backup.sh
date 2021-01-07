2ebec99f9ec234ca85b9bf2efc19a603d66a1297
LJLee37 <ljlee3759@gmail.com>
2021-01-07 14:15:44 +0000

    Modify backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 curdate=$(date +%F%Z)
 sudo tar -cvpzf backup-$curdate.tar.gz --exclude=/home/ljlee/backup*.tar.gz --one-file-system /
 sudo tar -cvpzf backup-firmware-$curdate.tar.gz --exclude=/home/ljlee/backup*.tar.gz --one-file-system /boot/firmware
-scp backup-$curdate.tar.gz 192.168.35.203:
-scp backup-firmware-$curdate.tar.gz 192.168.35.203:
+scp backup-$curdate.tar.gz 192.168.35.203:Downloads/
+scp backup-firmware-$curdate.tar.gz 192.168.35.203:Downloads/
 echo Backup complete!

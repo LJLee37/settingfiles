25f33f44b08cdc5c9dd64ef40cb74fe46e0dddd9
LJLee37 <ljlee3759@gmail.com>
2021-01-17 23:17:42 +0900

    Solve gpg issue

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install zsh tmux neovim ctags python3-neovim python3-pip htop keychain git g++ gdb neofetch -y
-
+git config --global user.name "LJLee37"
+git config --global user.email "ljlee3759@gmail.com"
+git config --global user.signingkey 8090E75C56E49652
+git config --global commit.gpgsign true
+git config --global gpg.program gpg

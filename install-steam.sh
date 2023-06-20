2630d60fc206c8cb9d15392051d3fdabd81042ec
LJLee37 <ljlee3759@gmail.com>
2023-06-20 23:22:35 +0900

    Update install steam

--- install-steam.sh
+++ install-steam.sh
@@ -3,4 +3,4 @@
 # echo [multilib] >> /etc/pacman.conf
 # echo Include = /etc/pacman.d/mirrorlist >> /etc/pacman.conf
 
-pacman -Syu steam vulkan-mesa-layers lib32-libva-mesa-driver lib32-mesa-vdpau lib32-dbus lib32-jack lib32-libavtp lib32-libsamplerate lib32-libpulse lib32-speexdsp lib32-vulkan-mesa-layers steam-native-runtime
+pacman -Syu steam vulkan-mesa-layers lib32-libva-mesa-driver lib32-mesa-vdpau lib32-dbus lib32-jack lib32-libavtp lib32-libsamplerate lib32-libpulse lib32-speexdsp lib32-vulkan-mesa-layers steam-native-runtime lib32-librsvg lib32-libva-intel-driver

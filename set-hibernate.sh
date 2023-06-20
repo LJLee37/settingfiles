e3cb85de651b1dd8b003935e3c22a8567cc338be
LJLee37 <ljlee3759@gmail.com>
2023-06-21 00:27:47 +0900

    Fix set-hibernate

--- set-hibernate.sh
+++ set-hibernate.sh
@@ -1,4 +1,4 @@
-sudo sed 's/filesystems fsck/filesystems resume fsck/' /etc/mkinitcpio.conf
+sudo sed -i -e 's/filesystems fsck/filesystems resume fsck/' /etc/mkinitcpio.conf
 sudo mkinitcpio -P
-sudo sed 's/quiet/quiet resume=UUID=bc22c8a5-306c-4ad3-81c3-753aff6f562d/' /etc/default/grub
+sudo sed -i -e 's/quiet/quiet resume=UUID=bc22c8a5-306c-4ad3-81c3-753aff6f562d/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
